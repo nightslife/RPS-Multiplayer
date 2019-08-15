52b09f9b24ce27b7c06819fc33fae30abb1a5583
nightslife <ragefail1212@gmail.com>
2019-08-15 16:52:40 -0400

    train logic working, no css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -26,14 +26,52 @@ $("#addTrain").on("click",function(){
     trainDest = $("#trainDest").val().trim()
     trainTime = $("#trainTime").val().trim()
     trainFreq = parseInt($("#trainFreq").val().trim())
-    database.ref().push({
-        trainName,
-        trainDest,
-        trainTime,
-        trainFreq
-    })
+    let trainData = []
+    trainData.push(trainDest, trainTime, trainFreq)
+    database.ref("/"+trainName).set(
+        trainData
+    )
     $("#trainName").val("")
     $("#trainDest").val("")
     $("#trainTime").val("")
     $("#trainFreq").val("")
 })
+
+database.ref().on("value", function(snap){
+    let trainNames = Object.keys(snap.val())
+    console.log(trainNames)
+    $("#deleteOnUpdate").remove()
+    for(trainTable of trainNames){
+        console.log(snap.val()[trainTable])
+        let addTrainRow = (snap.val()[trainTable])
+        let newRow = $("<tr>")
+        newRow.attr("id","deleteOnUpdate")
+        let newTrainName = $("<td>")
+        newTrainName.text(trainTable)
+        let newTrainDest = $("<td>")
+        newTrainDest.text(addTrainRow[0])
+        let nextComingTime = $("<td>")
+        nextComingTime.text(timeIncoming(addTrainRow[1],addTrainRow[2]))
+        newRow.append(newTrainName,newTrainDest,nextComingTime)
+        $("#trainTable").append(newRow)
+
+}
+})
+
+function timeIncoming (startTime, iteratedTime) {
+    let currTime = moment()
+    let startTrainTime = moment(startTime, "HH:mm")
+    let nextMinute = (startTrainTime.diff(currTime,"minutes"))
+    console.log(nextMinute)
+    console.log(iteratedTime)
+    let testingTime = moment(startTrainTime).add(iteratedTime,"m")
+    console.log(moment(testingTime).format("HH:mm"))
+    
+    while (nextMinute <= 0){
+        startTrainTime = moment(startTrainTime).add(iteratedTime,"m")
+        nextMinute = (startTrainTime.diff(currTime,"minutes"))
+        console.log(startTrainTime)
+        console.log("This is the new nextMinute: "+nextMinute)
+    }
+    return nextMinute
+}
